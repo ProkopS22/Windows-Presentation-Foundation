42ae18509f584f3a58980fed7e74d833899dc69d
ŠImon Prokop <ProkopS@spstrutnov.cz>
2024-01-15 12:24:59 +0100

    začátek

---
+++ WpfApp1/WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System.Reflection.Emit;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace WpfApp1
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+
+           
+        {
+            InitializeComponent();
+        }
+
+        private void NumberButton_Click(object sender, RoutedEventArgs e)
+        {
+            Button clickedButton = sender as Button;
+            if (clickedButton != null)
+            {
+                // Přidat obsah stisknutého tlačítka k obsahu labelu
+                label.Content += clickedButton.Content.ToString();
+            }
+
+
+        }
+    }
+}
